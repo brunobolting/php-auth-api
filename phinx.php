b9bbdbc710b1dc9c71dd24a911c171ce06596191
Bruno Bolting <brunoboltingx@gmail.com>
2022-09-11 23:17:07 -0300

    chore: add phinx

---
+++ phinx.php
@@ -0,0 +1,25 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/database/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/database/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'migrations',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'sqlite',
+            'name' => './database/app',
+            'suffix' => '.db',
+        ],
+        'testing' => [
+            'adapter' => 'sqlite',
+            'name' => 'app',
+            'mode' => 'memory',
+            'cache' => 'shared',
+        ]
+    ],
+    'version_order' => 'creation'
+];
